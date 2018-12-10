f1244973744a27fa7ebb0fbce935f6cdb46636c8
AgatheLagache <agathe.lagache@viacesi.fr>
2018-12-10 16:02:19 +0100

    Classes, Interfaces et Méthodes créées

---
+++ WpfApp1/WpfApp1/View/RestaurantView.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.View
+{
+    class RestaurantView : IRestaurantView, IRestaurantController
+    {
+        public Restaurant Restaurant { get => restaurant; set => restaurant = value; }
+
+        RestaurantView(Restaurant restaurant)
+        {
+
+        }
+
+        start_Scenario()
+        {
+
+        }
+
+
+    }
+}
