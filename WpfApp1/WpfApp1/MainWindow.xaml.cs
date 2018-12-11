2882f8253c8f249f4fc1cb868604704e1643bc15
AgatheLagache <agathe.lagache@viacesi.fr>
2018-12-11 08:53:58 +0100

    Vérification de l'entrée texte

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -21,6 +21,11 @@ namespace WpfApp1
     public partial class MainWindow : Window
     {
         Random nb_random = new Random();
+        int Nb_Fast_Clients = 1;
+        int Nb_Ordinary_Clients = 1;
+        int Nb_Slow_Clients = 1;
+        int Nb_Waiters = 1;
+        int Nb_Cooks = 1;
 
         public MainWindow()
         {
@@ -48,6 +53,58 @@ namespace WpfApp1
         private void Nb_Cooks_Random_Click(object sender, RoutedEventArgs e)
         {
             Nb_Cooks_Choice.Text = nb_random.Next(1, 12).ToString();
+        }
+
+        private void Check_Int_Validity(string Entered_Value, int Destination_Value)
+        {
+            if (int.TryParse(Entered_Value, out Destination_Value))
+            {
+
+            }
+            else
+            {
+                MessageBox.Show("Veuillez saisir un nombre valide entre 1 et 100.");
+            }
+        }
+
+        private void Nb_Fast_Clients_Choice_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                Check_Int_Validity(Nb_Fast_Clients_Choice.Text, Nb_Fast_Clients);
+            }           
+        }
+
+        private void Nb_Ordinary_Clients_Choice_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                Check_Int_Validity(Nb_Ordinary_Clients_Choice.Text, Nb_Ordinary_Clients);
+            }
+        }
+
+        private void Nb_Slow_Clients_Choice_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                Check_Int_Validity(Nb_Slow_Clients_Choice.Text, Nb_Slow_Clients);
+            }
+        }
+
+        private void Nb_Waiters_Choice_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                Check_Int_Validity(Nb_Waiters_Choice.Text, Nb_Waiters);
+            }
+        }
+
+        private void Nb_Cooks_Choice_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                Check_Int_Validity(Nb_Cooks_Choice.Text, Nb_Cooks);
+            }
         }
     }
 }
